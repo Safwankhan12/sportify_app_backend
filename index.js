94c682b0ac4323b1d5e80a6e86e172d2dd61117d
safwan <safwankhan525@gmail.com>
2024-09-05 00:11:29 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const cors = require('cors')
+const app = express();
+
+const port = 5000;  // changed from 3000 to 5000 because port 3000 will run react app
+
+// creating middlewhere to deal with json 
+app.use(express.json());
+app.use(cors())
+
+//available routes
+
+
+app.listen(port, () => {
+ console.log(`Example app listening on port ${port}`);
+});
