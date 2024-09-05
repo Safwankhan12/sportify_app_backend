c98ae287d68aa398dab7621dd74dae4bae017abe
safwan <safwankhan525@gmail.com>
2024-09-05 20:44:16 +0500

    added passport auth

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Db = require('./DatabaseConnection')
 Db()
 const express = require("express");
+import passport from 'passport';
+import session from 'express-session';
 const cors = require('cors')
 const app = express();
 
@@ -10,7 +12,23 @@ const port = 5000;  // changed from 3000 to 5000 because port 3000 will run reac
 app.use(express.json());
 app.use(cors())
 
+
+app.use(session({
+    secret: "safwan_dev",
+    saveUninitialized: false,
+    resave: false,
+    cookie:{
+        maxAge : 60000* 60
+    }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+
 //available routes
+app.use('/api/auth', require('./routes/auth'))
+
 
 app.listen(port, () => {
  console.log(`Example app listening on port ${port}`);
---
+++ strategies_passport/local-strategy.js
@@ -0,0 +1,9 @@
+// import passport from "passport";
+// import { Strategy } from "passport-local";
+// import {User} from "../models/user
+
+// passport.use(
+//     new Strategy({usernameField:'email'}, async(username, password, done)=>{
+//         const user = await
+//     })
+// )
