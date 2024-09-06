a77dff9830c49a58823da71522d53d1f12aa109b
safwan <safwankhan525@gmail.com>
2024-09-06 21:27:36 +0500

    added duplicate user check in add user

--- routes/user.js
+++ routes/user.js
@@ -10,12 +10,17 @@ router.post('/adduser',[
     body('password').isLength({min:6}).withMessage('Password should be atleast 6 characters')
 ],
 async (req,res)=>{
+    const user = await User.findOne({where : {email : req.body.email}})
+    if (user)
+    {
+        return res.status(404).json({error : "User already exists"})
+    }
     const errors = validationResult(req)
     if (!errors.isEmpty())
     {
         return res.status(400).json({errors: errors.array()})
     }
-    const NewUser = await User.create({
+     const NewUser = await User.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
